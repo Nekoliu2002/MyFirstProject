1549cbe9e63d84389d45e8795162238b99564b7f
Nekoliu2002 <104605673+Nekoliu2002@users.noreply.github.com>
2022-04-29 09:36:14 +0800

    Add files via upload

---
+++ Project/DoubleBeepers.py
@@ -0,0 +1,68 @@
+"""
+File: DoubleBeepers.py
+Name:
+-------------------------------
+TODO:
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel will double the beepers
+    """
+    move()
+    double_beepers()
+    move_back()
+    go_home()
+
+
+def double_beepers():
+    while on_beeper():
+        pick_beeper()
+        move()
+        put_beeper()
+        put_beeper()
+        turn_around()
+
+
+def move_back():
+    move()
+    while on_beeper():
+        pick_beeper()
+        turn_around()
+        put_beeper()
+        move()
+
+
+def turn_around():
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+
+
+def go_home():
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+
+
+
+
+
+
+
+
+
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
